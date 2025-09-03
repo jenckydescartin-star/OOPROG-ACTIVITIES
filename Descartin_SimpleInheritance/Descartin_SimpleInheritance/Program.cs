bcefcf703967be96c4423ad57155ade3b2f83294
ACT-STUDENT <ACT-STUDENT@LAB3-PC02>
2025-09-03 10:54:18 +0800

    Simple Inheritance with Properties

---
+++ Descartin_SimpleInheritance/Descartin_SimpleInheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Descartin_SimpleInheritance
+{
+    internal class Program
+    //Name: Jencky Descartin
+    //Section: IT301
+    //EXER6: Simple Inheritance with Properties
+    {
+        static void Main(string[] args)
+        {
+            RegularStudent regular = new RegularStudent
+            {
+                Name = "Jencky Descartin",
+                Program = "BSIT",
+                Section = "IT301A"
+            };
+            Console.WriteLine("Regular Stdent Info:");
+            regular.BasicInfo(); // Inherited Method
+            regular.SectionEnrolled(); // Regular Student Specific Method
+
+            Console.WriteLine();
+            IrregularStuddent irregular = new IrregularStuddent
+            {
+                Name = "Savana Mariano",
+                Program = "BEED",
+                UnitsEnrolled = "20"
+
+            };
+            Console.WriteLine("Irregular Student Info:");
+            irregular.BasicInfo(); // Inherited Method
+            irregular.UnitsSemEnrolled(); // Irregular Student Specific Method
+
+           
+            Console.ReadKey();
+
+
+        }
+
+        
+    }
+}
---
+++ Descartin_SimpleInheritance/Descartin_SimpleInheritance/Student.cs
@@ -0,0 +1,49 @@
+﻿using Descartin_SimpleInheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descartin_SimpleInheritance
+{
+     class Student
+    
+    {
+
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        public void BasicInfo() // Method to Display the info of the student
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Program: {Program}");
+        }
+
+    }
+}
+
+class RegularStudent : Student // Derived class
+{
+    public string Section { get; set; }
+
+    public void SectionEnrolled() // Method Specific to Irregular class
+    {
+        Console.WriteLine($"Section: {Section}");
+    }
+}
+
+class IrregularStuddent : Student
+{
+    public string UnitsEnrolled { get; set; }
+
+    public void UnitsSemEnrolled()
+    {
+        Console.WriteLine($"Units Enrolled this Semester: {UnitsEnrolled}");
+    }
+
+
+
+
+}
+
