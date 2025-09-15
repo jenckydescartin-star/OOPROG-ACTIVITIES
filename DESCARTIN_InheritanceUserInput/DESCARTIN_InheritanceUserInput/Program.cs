58a56088354fdcb481acbd8ffd088f836cc098cb
ACT-STUDENT <ACT-STUDENT@LAB3-PC04>
2025-09-15 10:57:54 +0800

    Exercise8

--- DESCARTIN_InheritanceUserInput/DESCARTIN_InheritanceUserInput/Program.cs
+++ DESCARTIN_InheritanceUserInput/DESCARTIN_InheritanceUserInput/Program.cs
@@ -19,25 +19,62 @@ namespace DESCARTIN_InheritanceUserInput
             Console.WriteLine("Choose a type of Jeans: ");
             Console.WriteLine("- SlimJeans ");
             Console.WriteLine("- FlaredJeans ");
-            Console.WriteLine("Enter your type");
+            Console.Write("Enter your type: ");
 
             int choice = Convert.ToInt32(Console.ReadLine());
 
-            //If the user chooses Slimbag
-            if (choice == 0) 
+            //If the user chooses SlimJeans
+            if (choice == 1)
             {
                 //User for slimjeans brand
-                Console.Write("Enter Brand : ");
+                Console.Write("Enter the Brand : ");
                 string brand = Console.ReadLine();
 
                 //User for slimjeans model
-                Console.Write();
+                Console.Write("Enter the model ");
                 string model = Console.ReadLine();
 
+                //User for jeans color
+                Console.Write("Enter your jeans size: ");
+                string color = Console.ReadLine();
+
                 //User for jeans size
-                Console.WriteLine("Enter your jeans size: ");
+                Console.Write("Enter the size(Small, Medium, Large: ");
+                string size = Console.ReadLine();
+                
+                SlimJeans slimjeans = new SlimJeans(model, color, brand, size);
+
+                //Clear the console screen 
+                Console.Clear();
 
             }
+            //If the user Chooses FlaredJeans
+            else if (choice == 2)
+            {
+                //User for flaredjeans type
+                Console.Write("Enter the color: ");
+                string color = Console.ReadLine();
+
+                //User for flaredjeans price
+                Console.Write("Enter the price: ");
+                string size = Console.ReadLine();
+
+
+
+                //Clear the console screen
+                Console.Clear();
+            }
+            // If thw user enters an invalid choice
+            else
+            {
+                //Clear the console and display error message
+                Console.Clear();
+                Console.WriteLine("Invalid Choices");
+            }
+            
+            //Wait for user to press a key before closing
+            Console.ReadKey();
+
         }
     }
 }
