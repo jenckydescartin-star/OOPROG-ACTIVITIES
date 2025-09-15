e6129dc20af71b535c9fe55b37d10df72244142a
ACT-STUDENT <ACT-STUDENT@LAB3-PC02>
2025-09-15 08:52:25 +0800

    user input

---
+++ DESCARTIN_InheritanceUserInput/DESCARTIN_InheritanceUserInput/Jeans.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESCARTIN_InheritanceUserInput
+{
+    class Jeans
+    {
+        public string brand;
+        public string model;
+
+        //Constructor for class method
+        public Jeans(string brand, string model)
+        {
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Model: " + model);
+        }
+        //Base class method
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Model: " + model);
+        }
+    }
+    class SlimJeans : Jeans //Derived Class
+    {
+        //Fields
+        public string color;
+        public string size;
+
+        //Constructor for slimjeans
+        public SlimJeans(string brand, string model, string color, string size) : base(brand, model)
+        {
+            this.brand = brand;
+            this.model = model;
+        }
+        //Method for slimjeans Derived Class
+        public void DisplayColorSlimJeans()
+        {
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Size: " + size);
+        }
+    }
+    class FlaredJeans : Jeans //Another one Derived Class
+    {
+        //Fields
+        public string type;
+        public int price;
+
+        //Constructor for FlaredJeans Derived Class
+        public FlaredJeans(string brand, string model, int price, string type) : base(brand, model)
+        {
+            this.type = type;
+            this.price = price;
+        }
+        // Method for flaredjeans Derived Class
+        public void DisplayFlaredJeans()
+        {
+            Console.WriteLine("Type: " + type);
+            Console.WriteLine("Price; " + price);
+        }
+
+    }
+}
---
+++ DESCARTIN_InheritanceUserInput/DESCARTIN_InheritanceUserInput/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESCARTIN_InheritanceUserInput
+{
+    internal class Program
+    {
+
+        //Jencky Descartin
+        //Section: IT301
+        //Exercise Title: Inheritance with User Input
+        //Main Method - Entry point of the program
+        static void Main(string[] args)
+        {
+            //Jeans User Input
+            Console.WriteLine("Choose a type of Jeans: ");
+            Console.WriteLine("- SlimJeans ");
+            Console.WriteLine("- FlaredJeans ");
+            Console.WriteLine("Enter your type");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            //If the user chooses Slimbag
+            if (choice == 0) 
+            {
+                //User for slimjeans brand
+                Console.Write("Enter Brand : ");
+                string brand = Console.ReadLine();
+
+                //User for slimjeans model
+                Console.Write();
+                string model = Console.ReadLine();
+
+                //User for jeans size
+                Console.WriteLine("Enter your jeans size: ");
+
+            }
+        }
+    }
+}
